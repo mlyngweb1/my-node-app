ff06399e387245751b127880df0df70423b1739c
MARYLYNG\maryl <mary.lyng.ml@gmail.com>
2026-01-28 17:33:35 +0000

    About page update

--- controllers/about.js
+++ controllers/about.js
@@ -6,7 +6,7 @@ import logger from "../utils/logger.js";
 const about = {
   createView(request, response) {
     logger.info("About page loading!");
-    response.send('About the Playlist app');   
+    response.send('About the Playlist app!');   
   },
 };
 
